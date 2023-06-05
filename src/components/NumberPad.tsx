d2d19801cb035ed0c6936ab4c629dc12ecd766f8
Shterion Stamboliev <shterion.stamboliev@gmail.com>
2023-06-05 20:40:40 +0300

    .

--- src/components/NumberPad.tsx
+++ src/components/NumberPad.tsx
@@ -3,13 +3,14 @@ import { useState, useEffect } from 'react';
 const NumberPad = () => {
 
     const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-    const dots = ['o', 'o', 'o', 'o', 'o', 'o']
-    const screenPassword = '123456'
+    const dots = ['o', 'o', 'o', 'o', 'o', 'o'];
+    const screenPassword = '123456';
 
     const [pressedNumber, setPressedNumber] = useState<number[]>([]);
     const [error, setError] = useState<string>('');
     const [isCorrect, setIsCorrect] = useState<boolean>(false);
-    const [color, setColor] = useState<string>('');
+    const [borderColor, setBorderColor] = useState<string>('white');
+    
 
     useEffect(() => {
         if (pressedNumber.length === 1) {
@@ -34,8 +35,9 @@ const NumberPad = () => {
     };
 
     const handleColorChange = () => {
-        
-    }
+            const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+            setBorderColor(randomColor);
+    };
 
     return (
         <>
@@ -45,7 +47,7 @@ const NumberPad = () => {
                         {error}
                     </div>
                 ) : <div className="grid-wrapper">
-                    <div className="dots" >
+                    <div className="dots" style={{ color: `${borderColor}` }}>
                         {dots.map((dot, i) => (
                             <div key={i}>{dot}</div>
                         ))}
@@ -68,7 +70,7 @@ const NumberPad = () => {
                             style={{
                                 background: 'none',
                                 bottom: '90px',
-                                left: '80px'
+                                left: '80px',
                             }}
                             onClick={deleteNum}>Delete</button>
                         <button className="clr-btn"
